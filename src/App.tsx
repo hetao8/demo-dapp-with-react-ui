3b3a6ccd938b538a9ae037e45fae796e6397d32b
hetao8 <hetao@valleysound.xyz>
2025-03-26 15:53:30 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -310,19 +310,10 @@ function App() {
                 imageUrl: "https://cdn.echooo.xyz/front-end/fw/2025-01-20/f27b7b41-66b4-4b5d-b3d4-a3ac1d8b34ba.png",
                 aboutUrl: "https://www.echooo.xyz",
                 universalLink: "https://echooo-ton.vercel.app",
+                bridgeUrl: "https://ton-connect-bridge.echooo.link/bridge",
                 deepLink: "echooo://echooo.valleysound.xyz/vss/ton-connect",
+                jsBridgeKey: 'echoooTonWallet',
                 platforms: ["ios", "android", "macos", "windows", "linux"],
-                // @ts-ignore
-                bridge: [
-                  {
-                      type: 'js',
-                      key: 'echoooTonWallet'
-                  },
-                  {
-                      type: 'sse',
-                      url: 'https://ton-connect-bridge.echooo.link/bridge'
-                  }
-              ],
               }
             ]
           }}
